d94eb22c484b3a3baea7678506e84d505fab3169
Radhin-Radhakrishnan <radhinpzr@gmail.com>
2025-04-19 10:47:23 +0530

    Skelton applied

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,8 @@
 "use client";
-import React from "react";
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {Box, Button, Card, CardActions, CardContent, CardMedia, Typography, useMediaQuery, useTheme,} from "@mui/material";
 import Image from "next/image";
+import Skeleton from '@mui/material/Skeleton';
 import SliderComponent from "@/components/slider/SliderComponent";
 import MainInterfaceSlider from "@/components/slider/MainInterfaceSlider";
 import Sidebar from "@/components/sidebar/Sidebar";
@@ -25,10 +16,20 @@ export default function Home() {
   const { isSidebarOpen } = useSelector((state: RootState) => state.header);
 
   const theme = useTheme()
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
-  
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500); // 1s delay
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    
+
     <Box
       sx={{
         // width: "100%"
@@ -36,7 +37,7 @@ export default function Home() {
         flexDirection: "column",
       }}
     >
-      
+
       <Box
         sx={{
           display: "flex",
@@ -45,27 +46,58 @@ export default function Home() {
         }}
       >
         {
-          isSidebarOpen  ? (
+          isSidebarOpen ? (
             <Box
-            component="aside"
-            sx={{
-              width: { xs: "100%", sm: "30%", md: "25%" }, // Full-width on small screens, 25% on md+
-              flexShrink: 0, // Prevent sidebar from shrinking
-              display: { xs: "none",sm: "none", md: "block" }, // Hide sidebar on extra-small screens
-            }}
-            height="495px"
-          >
-            <Sidebar />
-          </Box>
+              component="aside"
+              sx={{
+                width: { xs: "100%", sm: "30%", md: "25%" }, // Full-width on small screens, 25% on md+
+                flexShrink: 0, // Prevent sidebar from shrinking
+                display: { xs: "none", sm: "none", md: "block" }, // Hide sidebar on extra-small screens
+              }}
+              height="495px"
+            >
+              {loading ? (
+                <Skeleton variant="rectangular" height="100%" animation="wave" sx={{   bgcolor: 'blue.300', borderRadius: 2, boxShadow: 2, border: '1px solid #ccc' }} />
+              ) : (
+
+                <Sidebar />
+              )}
+
+            </Box>
           ) : null
         }
-       
-        
-        <MainInterfaceSlider />
+        {loading ? (
+          <Skeleton
+            variant="rectangular"
+            width="100%"
+            height={500}
+            animation="wave"
+            sx={{  bgcolor: 'grey.300', borderRadius: 2, boxShadow: 2, border: '1px solid #ccc'}}
+          />
+        ) : (
+
+          <MainInterfaceSlider />
+        )}
       </Box>
-      <FeaturesList />
-      <SliderComponent />
-      
+
+      <Box sx={{ mt: 3 }}>
+        {loading ? (
+          <>
+            <Skeleton variant="text" width="60%" height={30} animation="wave" sx={{ my: 2, bgcolor: 'grey.300', borderRadius: 2, boxShadow: 2, border: '1px solid #ccc'}} />
+            <Skeleton variant="rectangular" height={120} animation="wave" sx={{ my: 2, bgcolor: 'grey.300', borderRadius: 2, boxShadow: 2, border: '1px solid #ccc'}} />
+          </>
+        ) : (
+          <FeaturesList />
+        )}
+      </Box>
+
+      <Box sx={{ mt: 3 }}>
+        {loading ? (
+          <Skeleton variant="rectangular" height={200} animation="wave" sx={{ my: 2, bgcolor: 'grey.300', borderRadius: 2, boxShadow: 2, border: '1px solid #ccc'}} />
+        ) : (
+          <SliderComponent />
+        )}
+      </Box>
     </Box>
   );
 }
